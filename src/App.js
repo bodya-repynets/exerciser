18097c493ff95c9211410ad02d34e23581bd4d33
bodya-repynets <bohdanrepynets@gmail.com>
2023-05-30 11:31:12 +0300

    created home structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Main from "./pages/Main";
+import Exercise from "./containers/Exercise";
+import Home from "./containers/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route index element={<Home />} />
+          <Route path="/exercises/:id" element={<Exercise />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
