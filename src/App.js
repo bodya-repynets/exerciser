938b5dcaa3648534db8e3b0f6b948bb2fc7c2cb0
bodya-repynets <bohdanrepynets@gmail.com>
2023-08-09 13:06:39 +0300

    fix deploy

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import Main from "./pages/Main";
 import Exercise from "./pages/Exercise";
 import Home from "./containers/Home";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <BrowserRouter basename="/exerciser">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />}>
           <Route index element={<Home />} />
